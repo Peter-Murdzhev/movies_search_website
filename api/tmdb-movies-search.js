406cc427c553abe7e3491c740a590d6a994a771b
Peter <murdzhevpetar@gmail.com>
2025-12-17 17:05:06 +0200

    fixed tiny fetch syntax

--- api/tmdb-movies-search.js
+++ api/tmdb-movies-search.js
@@ -1,19 +1,20 @@
 export default async function handler(req, res) {
     const apiKey = process.env.TMDB_TOKEN;
-    const query = req.query;
+    const { query } = req.query;
 
     try {
         const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}`, {
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer ${apiKey}`
-        }})
+            headers: {
+                accept: "application/json",
+                Authorization: `Bearer ${apiKey}`
+            }
+        })
         const data = await response.json();
 
         res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({error: "Error fetching movies"})
+        res.status(500).json({ error: "Error fetching movies" })
     }
 
 }
