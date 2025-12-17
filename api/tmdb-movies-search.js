87cea9c1224e687ed5971b14e710459c773d4f21
Peter <murdzhevpetar@gmail.com>
2025-12-17 17:19:43 +0200

    fixed res.json()

--- api/tmdb-movies-search.js
+++ api/tmdb-movies-search.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         const data = await response.json();
 
         res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
-        res.status(200).json(data);
+        res.status(200).json(data.results);
     } catch (error) {
         res.status(500).json({ error: "Error fetching movies" })
     }
