74e2f3eabe2db1582a91179886b3449888d52d85
Peter <murdzhevpetar@gmail.com>
2025-12-26 19:30:38 +0200

    fixing mobile routing

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const MovieCard = ({ movie }) => {
+  const navigate = useNavigate();
 
   return (
-    <Link to={`/movie/${movie.id}`} className="movie_card">
+    <div className="movie_card" onClick={() => navigate(`/movie/${movie.id}`)}>
       <h3>{movie.title}</h3>
       <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
       <p style={{ color: "yellow", textAlign: "center" }}><strong>Rating:{movie.vote_average}</strong></p>
       <p className="release_date">{movie.release_date}</p>
-    </Link>
+    </div>
   )
 }
 
