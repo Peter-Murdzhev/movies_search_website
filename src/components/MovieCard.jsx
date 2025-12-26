6885b0cea967beed0fba2b275af3ed954b5639f5
Peter <murdzhevpetar@gmail.com>
2025-12-26 18:41:07 +0200

    fixing mobile routing

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,14 @@ const MovieCard = ({ movie }) => {
 
   return (
 
-    <div className="movie_card">
-      <Link to={`/movie/${movie.id}`}>
-        <h3>{movie.title}</h3>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}></img>
-        <p style={{ color: "yellow", textAlign: "center" }}><strong>Rating:{movie.vote_average}</strong></p>
-        <p className="release_date">{movie.release_date}</p>
-      </Link>
-    </div >
+
+    <Link to={`/movie/${movie.id}`} replace className="movie_card">
+      <h3>{movie.title}</h3>
+      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}></img>
+      <p style={{ color: "yellow", textAlign: "center" }}><strong>Rating:{movie.vote_average}</strong></p>
+      <p className="release_date">{movie.release_date}</p>
+    </Link>
+
 
 
   )
