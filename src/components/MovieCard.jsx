b35433a34ce12f747b93bfcf27e58d0de04db82f
Peter <murdzhevpetar@gmail.com>
2025-12-26 19:07:22 +0200

    fixing css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const MovieCard = ({ movie }) => {
 
   return (
-    <Link to={`/movie/${movie.id}`} className="movie_card">
+    <Link to={`/movie/${movie.id}`} replace className="movie_card">
       <h3>{movie.title}</h3>
       <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
       <p style={{ color: "yellow", textAlign: "center" }}><strong>Rating:{movie.vote_average}</strong></p>
